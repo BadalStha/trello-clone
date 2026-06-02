e892c993630b98f227979780ec03ec41174bf7c4
Badal Shrestha <shresthabadal324@gmail.com>
2026-06-02 14:43:41 +0545

    Displayed projects from database on the web page

--- boards/views.py
+++ boards/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Project
 
 # Create your views here.
+
+def project_list(request):
+    # Fetch all projects from database
+    all_projects = Project.objects.all()
+
+    # Send those projects to an html file
+    return render(request, 'boards/project_list.html', {'projects': all_projects})
